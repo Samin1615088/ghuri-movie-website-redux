08540e8c9c4b56fcb5f115e474edd15d9e9bdfd4
Samin1615088 <samin.shafkat@gmail.com>
2021-05-16 19:03:52 +0600

    title button styles added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,19 +7,19 @@ function Banner() {
             className="banner"
             style={{
                 backgroundSize: 'cover',
-                backgroundImage: `url(https://wealthlab.co/wp-content/uploads/2018/08/Netflix_logo.svg_.png)`,
+                backgroundImage: `url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC)`,
+                // backgroundImage: `url(https://wealthlab.co/wp-content/uploads/2018/08/Netflix_logo.svg_.png)`,
                 backgroundPosition: 'center center',
             }}>
             <div className="banner_contents">
-                <h1 className="banner_title"></h1>
-                <button className="banner_button">Play</button>
-                <button className="banner_button">My List</button>
-                <h1 className="banner_description">This is a test description</h1>
-
-                <div className="banner--fadeBottom">
-
+                <h1 className="banner_title">This is test title</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
                 </div>
+                <h1 className="banner_description">This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description </h1>
             </div>
+            {/* <div className="banner_fadeBottom" /> */}
 
         </header>
     )
