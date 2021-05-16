d9503aeead8cf66e837a6a91bf82c2a7aed2855b
Samin1615088 <samin.shafkat@gmail.com>
2021-05-16 12:16:49 +0600

    comments updated

---
+++ src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+import './HomeScreen.css'
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            {/* Nav bar */}
+            <NavBar />
+
+            {/* banner */}
+
+            {/* rows  */}
+        </div>
+    )
+}
+
+export default HomeScreen
