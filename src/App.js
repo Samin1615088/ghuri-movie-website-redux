51d838479b60f711efde2287522a5379b791b1f8
Samin1615088 <samin.shafkat@gmail.com>
2021-05-16 12:14:56 +0600

    blank HomeScreen and NavBar component is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './components/HomeScreen/HomeScreen';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build Ghurit website</h1>
+      <HomeScreen/>
     </div>
   );
 }
