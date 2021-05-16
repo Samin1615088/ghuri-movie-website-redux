ee5e00bc78657715fba11b59c7a267df45f2574f
Samin1615088 <samin.shafkat@gmail.com>
2021-05-16 18:23:24 +0600

    dummy banner content added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -10,6 +10,16 @@ function Banner() {
                 backgroundImage: `url(https://wealthlab.co/wp-content/uploads/2018/08/Netflix_logo.svg_.png)`,
                 backgroundPosition: 'center center',
             }}>
+            <div className="banner_contents">
+                <h1 className="banner_title"></h1>
+                <button className="banner_button">Play</button>
+                <button className="banner_button">My List</button>
+                <h1 className="banner_description">This is a test description</h1>
+
+                <div className="banner--fadeBottom">
+
+                </div>
+            </div>
 
         </header>
     )
