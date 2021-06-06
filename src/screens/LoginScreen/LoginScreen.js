7f34916842d9ca2a0c9cc3b4f7131d590ab1f0d0
Samin1615088 <samin.shafkat@gmail.com>
2021-06-06 15:41:05 +0600

    added

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './LoginScreen.css';
+import logo from './../../images/logo.png';
 const LoginScreen = () => {
     return (
         <div className="loginScreen">
-            <div className="loginScreen_background">
+            <div className="loginScreen__background">
+                <img
+                    className="loginScreen__logo"
+                    src={logo}
+                    alt=""
+                />
+                <button
+                    className="loginScreen__button"
+                    onClick={() => console.log("clicked")}
+                >Sign In</button>
 
+                <div className="loginScreen__gradient" />
+            </div>
+            {/* put below things over loginScreen__background */}
+            <div className="loginScreen__body">
+                <h1>Unlimited movies, TV shows, and more.</h1>
+                <h2>Watch anywhere. Cancel anytime.</h2>
+                <h4><span>Ready to watch?  </span> Enter your email to create or restart your membership.
+</h4>
             </div>
         </div>
     );
