254278c379b6249163ffeafe3b0cfc27930f82bf
Samin1615088 <samin.shafkat@gmail.com>
2021-06-07 22:07:53 +0600

    updated

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -12,6 +12,8 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
                 const response = await axios.get(fetchUrl);
                 console.log('current response', response);
                 setMovies(response?.data.results);
+                // const shuffledArray = shuffleArray(movies)
+                // setMovies(shuffledArray)
                 return response;
             } catch (error) {
                 console.error(error);
@@ -21,6 +23,22 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
     }, [fetchUrl])
     console.log('moives', movies);
 
+    // const shuffleArray = array => {
+    //     const currentIndex = array.length;
+    //     let randomIndex;
+
+    //     // While there remain elements to shuffle...
+    //     while (0 !== currentIndex) {
+    //         // Pick a remaining element...
+    //         randomIndex = Math.floor(Math.random() * currentIndex);
+    //         currentIndex--;
+
+    //         // And swap it with the current element.
+    //         [array[currentIndex], array[randomIndex]] = [
+    //             array[randomIndex], array[currentIndex]];
+    //     }
+    //     return array;
+    // }
     return (
         <div className="row">
             <h2>{title}</h2>
